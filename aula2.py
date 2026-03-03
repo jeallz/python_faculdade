56e107c1c6fc0671fc5967ac89eafd086c768da3
Estevan C. <121241412+jeallz@users.noreply.github.com>
2026-03-03 19:50:31 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,34 @@
+numero_1 = int(input("Digite um numero: "))
+numero_2 = int(input("Digite outro numero: "))
+
+if (numero_1 > numero_2):
+    print(f"O número {numero_1} é maior que o número {numero_2}")
+else:
+    print(f"O número {numero_1} é menor que o número {numero_2}")
+
+    
+# Exercício 2
+
+numero_3 = int(input("Digite um numero: "))
+
+print(numero_3 >= 10 and numero_3 <= 20) 
+
+# exercício 3 
+
+numero_4 = int(input("Digite um numero: "))
+numero_5 = int(input("Digite outro numero: "))
+
+print(numero_4 >= 100 or numero_5 >= 100)
+
+# exercício 4
+
+numero_6 = int(input("Digite um numero: "))
+
+print(not (numero_6 == 0))
+
+# exercício 5
+
+numero_7 = int(input("Digite um numero: "))
+numero_8 = int(input("Digite outro numero: "))
+
+print(numero_7 == numero_8)
